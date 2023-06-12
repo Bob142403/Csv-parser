04123d03d203eee8525c916467c84d2d5f15251c
Bob142403 <boboshka2006@mail.ru>
2023-06-12 21:15:34 +0500

    HEllo

--- src/App.vue
+++ src/App.vue
@@ -4,64 +4,68 @@ import { ref } from "vue";
 import { useToast } from "vue-toastification";
 
 const data = ref([]);
-const url = ref();
 const toast = useToast();
 
-function res(event) {
+function uploadFile(event) {
   const file = event.target.files[0];
   Papa.parse(file, {
     skipEmptyLines: true,
     complete(res) {
-      console.log(res);
       if (res.errors[0]) toast.error(res.errors[0].message);
       data.value = res.data;
+      console.log(data.value);
     },
   });
 }
 
-function download() {
+function downloadFile() {
+  const a = document.createElement("a");
+
   const str = Papa.unparse(data.value);
 
   const blob = new Blob([str], { type: "text/csv" });
 
-  url.value = window.URL.createObjectURL(blob);
+  const url = window.URL.createObjectURL(blob);
+
+  a.href = url;
+  a.download = "download.csv";
+
+  a.click();
 }
 </script>
 
 <template>
-  <div class="px-2 pt-3 flex flex-col">
+  <div class="px-2 pt-3 flex flex-col h-screen">
     <div class="flex justify-between">
       <input
         class="text-sm text-gray-900 w-11/12 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
         type="file"
-        @change="res"
+        @change="uploadFile"
       />
-      <a :href="url" download="download.csv">
-        <button
-          type="button"
-          :disabled="data"
-          @click="download"
-          class="ml-2 text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      <button
+        type="button"
+        :disabled="!data.length"
+        @click="downloadFile"
+        class="ml-2 text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        <svg
+          fill="none"
+          stroke="currentColor"
+          stroke-width="1.5"
+          viewBox="0 0 24 24"
+          class="w-5 h-5"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
         >
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-width="1.5"
-            viewBox="0 0 24 24"
-            class="w-5 h-5"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
-            ></path>
-          </svg>
-        </button>
-      </a>
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+          ></path>
+        </svg>
+      </button>
     </div>
-    <div class="relative overflow-x-auto my-4">
+    <div class="relative overflow-x-auto my-4 overflow-y-auto h-screen">
       <table
         class="w-full text-sm text-center text-gray-500 dark:text-gray-400"
       >
