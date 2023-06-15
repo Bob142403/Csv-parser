ace2760f08ee95f96f74b0c9c21e8952ad6aaa30
Bob142403 <boboshka2006@mail.ru>
2023-06-15 23:30:01 +0500

    Test

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import Papa from "papaparse";
 import { ref } from "vue";
 import { useToast } from "vue-toastification";
 
+import Modal from "./components/Modal.vue";
+
 const data = ref([]);
 const toast = useToast();
 
@@ -24,8 +26,11 @@ function uploadFile(event) {
     complete(res) {
       console.log(res);
       if (res.errors[0]) toast.error(res.errors[0].message);
-      data.value = res.data;
-      data.value.push(data.value[0].map((elem) => ""));
+      else {
+        data.value = res.data;
+        data.value.push(data.value[0].map((elem) => ""));
+        document.getElementById("open_modal").click();
+      }
     },
   });
 }
@@ -47,13 +52,49 @@ function downloadFile() {
 </script>
 
 <template>
-  <div class="px-2 pt-3 flex flex-col h-screen">
-    <div class="flex justify-between">
+  <Modal :table="data" />
+  <div class="flex items-center justify-center h-screen">
+    <label
+      for="dropzone-file"
+      class="mx-10 flex flex-col items-center justify-center w-96 h-96 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+    >
+      <div class="flex flex-col items-center justify-center pt-5 pb-6">
+        <svg
+          aria-hidden="true"
+          class="w-10 h-10 mb-3 text-gray-400"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+          ></path>
+        </svg>
+        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+          <span class="font-semibold">Click to upload</span> or drag and drop
+        </p>
+        <p class="text-xs text-gray-500 dark:text-gray-400">Only CSV files</p>
+      </div>
       <input
-        class="text-sm text-gray-900 w-11/12 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+        id="dropzone-file"
         type="file"
         @change="uploadFile"
+        class="hidden"
       />
+    </label>
+  </div>
+  <button
+    id="open_modal"
+    class="hidden"
+    data-modal-target="extralarge-modal"
+    data-modal-toggle="extralarge-modal"
+  ></button>
+  <!-- <div class="px-2 pt-3 flex flex-col h-screen">
+    <div class="flex justify-between">
       <button
         type="button"
         :disabled="!data.length"
@@ -105,11 +146,7 @@ function downloadFile() {
           >
             <template v-if="index != 0">
               <td class="border-r-2 px-1">{{ index }}</td>
-              <td
-                v-for="(elem, i) in row"
-                :key="i"
-                class="px-4 py-3 border-r-2"
-              >
+              <td v-for="(elem, i) in row" :key="i" class="border-r-2">
                 <input
                   class="pl-1 w-full"
                   :value="data[index][i]"
@@ -129,5 +166,5 @@ function downloadFile() {
         </tbody>
       </table>
     </div>
-  </div>
+  </div> -->
 </template>
